fd7b46a07c5ffbe0b2ca0362d9a8ccf9d3a7d557
Livia <Livia Chirica>
2015-08-29 16:26:00 +0300

    added new data to test.py

--- test.py
+++ test.py
@@ -31,7 +31,9 @@ if __name__ == '__main__':
 	categorii_list = [{'name':'Robotică'}, {'name':'Fizică'}, {'name':'Astronomie'}, {'name':'Informatică'}, {'name':'Matematică'}, {'name':'Sănătate'}, {'name':'Ecologie'}, {'name':'Erudiție'}, {'name':'Voluntariat'}, {'name':'Limbi străine'}, {'name':'Literatură'}, {'name':'Muzică'}, {'name':'SPort'}, {'name':'Artă plastică'}, {'name':'Artă dramatică'}]
 	target_group_list = [{'name':'Elevi gimnaziu'}, {'name':'Elevi liceu'}, {'name':'Studenti'},{'name':'Voluntari'}]
 	event_type_list = [{'name':'Workshop'}, {'name':'Training'}, {'name':'Proiect'}, {'name':'Olimpiada'}, {'name':'Club'}, {'name':'Curs'}, {'name':'Concurs'}, {'name':'Conferinta'}, {'name':'Tabara de vara'}]
+	user_type_list = [{'name':'ONG'}, {'name':'Persoana fizica'}, {'name':'Asociatie obsteasca'}]
 	insert_to_db(serialize_models(TargetGroup, target_group_list))
 	insert_to_db(serialize_models(TypeEvent, event_type_list))
 	insert_to_db(serialize_models(District, district_list))
 	insert_to_db(serialize_models(Category, categorii_list))
+	insert_to_db(serialize_models(UserType, user_type_list))
